b34c6d5c2afa5dee217a475a47dc0438fd42f4fa
gregpaton08 <gregpaton08@gmail.com>
2013-03-02 22:31:34 -0500

    Initial Commit

---
+++ CPGrapher/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CPGrapher
+//
+//  Created by Greg Paton on 3/2/13.
+//  Copyright (c) 2013 Greg Paton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CPGrapher/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CPGrapher
+//
+//  Created by Greg Paton on 3/2/13.
+//  Copyright (c) 2013 Greg Paton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
